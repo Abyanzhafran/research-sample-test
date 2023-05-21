83c9b5e9685ae20fc8fce12bfa7090e5d9440d3d
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-05-21 11:29:39 +0700

    project: file-sharing-node-js - set ignoreBinPackage in depcheck-perf-test to false - add const ejs, for requiring 'ejs' package, the purpose of this code is, because right now depcheck only can detect usage of direct package using cons 'package' or import, depend on your js-code-style. So it's just to equalize

--- MODIFIED/file-sharing-node-js/server.js
+++ MODIFIED/file-sharing-node-js/server.js
@@ -1,59 +1,60 @@
-require("dotenv").config()
-const multer = require("multer")
-const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
-const File = require("./models/File")
+require("dotenv").config();
+const multer = require("multer");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const File = require("./models/File");
+const ejs = require("ejs");
 
-const express = require("express")
-const app = express()
-app.use(express.urlencoded({ extended: true }))
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-const upload = multer({ dest: "uploads" })
+const upload = multer({ dest: "uploads" });
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL);
 
-app.set("view engine", "ejs")
+app.set("view engine", ejs);
 
 app.get("/", (req, res) => {
-  res.render("index")
-})
+  res.render("index");
+});
 
 app.post("/upload", upload.single("file"), async (req, res) => {
   const fileData = {
     path: req.file.path,
     originalName: req.file.originalname,
-  }
+  };
   if (req.body.password != null && req.body.password !== "") {
-    fileData.password = await bcrypt.hash(req.body.password, 10)
+    fileData.password = await bcrypt.hash(req.body.password, 10);
   }
 
-  const file = await File.create(fileData)
+  const file = await File.create(fileData);
 
-  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
-})
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
+});
 
-app.route("/file/:id").get(handleDownload).post(handleDownload)
+app.route("/file/:id").get(handleDownload).post(handleDownload);
 
 async function handleDownload(req, res) {
-  const file = await File.findById(req.params.id)
+  const file = await File.findById(req.params.id);
 
   if (file.password != null) {
     if (req.body.password == null) {
-      res.render("password")
-      return
+      res.render("password");
+      return;
     }
 
     if (!(await bcrypt.compare(req.body.password, file.password))) {
-      res.render("password", { error: true })
-      return
+      res.render("password", { error: true });
+      return;
     }
   }
 
-  file.downloadCount++
-  await file.save()
-  console.log(file.downloadCount)
+  file.downloadCount++;
+  await file.save();
+  console.log(file.downloadCount);
 
-  res.download(file.path, file.originalName)
+  res.download(file.path, file.originalName);
 }
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
