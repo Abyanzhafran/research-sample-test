3b20efcdef1647c1a74bc93cb170da71dfee02a9
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-05-27 11:41:19 +0700

    project: add videoSearch in modified folder - update all dependencies to latest version - add depcheck-perf-test - add deps-checker-perf-test

---
+++ MODIFIED/videoSearch/src/components/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit(
+      ' Indian National Anthem playing at Neeraj Chopra’s Victory Ceremony at #Tokyo2020!'
+    );
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
