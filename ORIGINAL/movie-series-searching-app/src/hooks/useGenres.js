3ec7db8fce9eac08f868f49f1efa655bea4ea6e1
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-08-10 22:43:32 +0700

    init commit

--- ORIGINAL/movie-series-searching-app/src/hooks/useGenres.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function useGenres(selectedGenres) {
-  if (selectedGenres < 1) return "";
-  const genreId = selectedGenres.map((g) => g.id);
-  return genreId.reduce((acc, curr) => acc + "," + curr);
-}
-
-export default useGenres;
--- ORIGINAL/react-query-crash-course-example/client/src/api/users.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios"
-
-export function getUser(id) {
-  return axios.get(`http://localhost:3000/users/${id}`).then(res => res.data)
-}
